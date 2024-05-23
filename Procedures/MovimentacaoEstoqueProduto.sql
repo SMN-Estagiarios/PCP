55ab6d7419231bc6e2bdb7417c6fb0da4b94a08f
OlivioFreitas <ojfneto92@gmail.com>
2024-05-23 10:42:50 -0300

    feat: cria proc de listar movimentacao estoque produto

--- Procedures/MovimentacaoEstoqueProduto.sql
+++ Procedures/MovimentacaoEstoqueProduto.sql
@@ -78,3 +78,65 @@ CREATE OR ALTER PROCEDURE [dbo].[SP_InserirMovimentacaoEstoqueProduto]
 			RETURN 5
 	END
 GO
+
+
+
+CREATE OR ALTER PROCEDURE [dbo].[SP_ListarMovimentacaoEstoqueProduto]
+	@IdMovimentacaoProduto INT = NULL
+	AS
+	/*
+	Documentacao
+	Arquivo Fonte.....: MovimentacaoEstoqueProduto.sql
+	Objetivo..........: Gerar um insert para a tabela de movimentacao no estoque da materia prima.
+	Autor.............: Olívio Freitas
+	Data..............: 23/05/2024
+	Ex................: BEGIN TRAN
+							DBCC FREEPROCCACHE
+							DBCC FREESYSTEMCACHE('ALL')
+							DBCC DROPCLEANBUFFERS
+
+							DECLARE @Data_Inicio DATETIME = GETDATE(),
+									@Retorno INT;
+
+							SELECT	Id,
+									IdTipoMovimentacao,
+									IdEstoqueProduto,
+									DataMovimentacao,
+									Quantidade
+								FROM [dbo].[MovimentacaoEstoqueProduto]
+
+							EXEC @Retorno = [dbo].[SP_ListarMovimentacaoEstoqueProduto]
+
+							SELECT	Id,
+									IdTipoMovimentacao,
+									IdEstoqueProduto,
+									DataMovimentacao,
+									Quantidade
+								FROM [dbo].[MovimentacaoEstoqueProduto]
+
+							SELECT	@Retorno AS Retorno,
+									DATEDIFF(MILLISECOND, @Data_Inicio, GETDATE());
+						ROLLBACK TRAN
+
+	Retornos..........: 0 - Sucesso.
+						1 - Erro, movimentacao inexistente
+	*/
+	BEGIN
+		IF @IdMovimentacaoProduto IS NOT NULL AND NOT EXISTS	(SELECT TOP 1 1
+																	FROM [dbo].[MovimentacaoEstoqueProduto] WITH(NOLOCK)
+																	WHERE Id = @IdMovimentacaoProduto)
+			BEGIN
+				RETURN 1
+			END
+
+		SELECT	Id,
+				IdTipoMovimentacao,
+				IdEstoqueProduto,
+				DataMovimentacao,
+				Quantidade
+			FROM [dbo].[MovimentacaoEstoqueProduto] WITH(NOLOCK)
+			WHERE Id = COALESCE(@IdMovimentacaoProduto, Id)
+
+		RETURN 0
+	END
+GO
