322dec05c6eea8f0b8afbac9a8c3b8b4a8025045
IsabellaSiqueira <isabella.tragante@hotmail.com>
2024-05-24 12:09:20 -0300

    REFACTOR: Refatorando gerar movimentacao nos produtos por entrega do pedido

--- Triggers/TRG_GerarMovimentacaoPorEntrega.sql
+++ Triggers/TRG_GerarMovimentacaoPorEntrega.sql
@@ -9,8 +9,8 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_GerarMovimentacaoPorEntregaDePedido]
 								IdTipoMovimentacao travado em 2 para categorizar saida de produtos do estoque mediante entrega do pedido 
 								quando o mesmo é entregue ao cliente
 		Autor................:	Adriel Alexander
-		Data.................:	23/05/2024
-		Autores Alteracao....:  Adriel Alexander
+		Data.................:	24/05/2024
+		Autores Alteracao....:  Gustavo Targino
 		Ex...................:  BEGIN TRAN
 									DBCC DROPCLEANBUFFERS;
 									DBCC FREEPROCCACHE;
@@ -42,28 +42,33 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_GerarMovimentacaoPorEntregaDePedido]
 				@IdEstoqueProduto INT,
 				@Quantidade INT,
 				@IdPedido INT,
+				@IdProduto INT,
 				@DataEntregaInserted DATE,
 				@DataEntregaDeleted DATE
 			
 		-- atribuicoes de valores para as variaveis 
 		SELECT @DataEntregaInserted = DataEntrega,
 			   @IdPedido = Id
-			FROM Inserted
+			FROM INSERTED
 		
 		SELECT @DataEntregaDeleted = DataEntrega
-			FROM deleted
-
+			FROM DELETED
 
 		--verifica se o motivo do update foi a realizacao da entrega dos produtos aos clientes
 		IF @DataEntregaInserted IS NOT NULL AND @DataEntregaDeleted IS NULL
 			BEGIN 
 			
-				INSERT INTO [dbo].[MovimentacaoEstoqueProduto] 
-					  SELECT   pp.IdProduto,
-							   2,
-							   @DataAtual,
-							   pp.Quantidade
-							FROM [dbo].[PedidoProduto] pp WITH(NOLOCK)
-							WHERE pp.IdPedido = @IdPedido
+				SELECT  @IdPedido = i.Id,
+                		@IdProduto = pp.IdProduto,
+                		@Quantidade = pp.Quantidade,
+                		@DataEntregaInserted = i.DataEntrega
+            FROM INSERTED i
+                INNER JOIN [dbo].[PedidoProduto] pp WITH(NOLOCK)
+                    ON @IdPedido = pp.IdPedido
+            WHERE i.DataEntrega IS NOT NULL AND @DataEntregaDeleted IS NULL
+
+			-- Inserir movimenta;áo em etoque produto com retirada de estoque
+			EXEC [dbo].[SP_InserirMovimentacaoEstoqueProduto] @IdProduto, 2, @DataAtual, @Quantidade
+
 			END
 	END	
