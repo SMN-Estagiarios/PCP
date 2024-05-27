e7cd9f51bd1f5194e54189dcac12f0e0711a2192
Danyel Targino <dantargino@outlook.com>
2024-05-27 11:06:04 -0300

    Fix - correcao no arquivo FNC_CalcularTempoProducaoProduto.

--- Functions/FNC_CalcularTempoProducaoProduto.sql
+++ Functions/FNC_CalcularTempoProducaoProduto.sql
@@ -12,19 +12,19 @@ CREATE OR ALTER FUNCTION [dbo].[FNC_CalcularTempoProducaoProduto](@Id INT)
 						DBCC FREEPROCCACHE;
 						
 						DECLARE @DataInicio DATETIME = GETDATE()
-						SELECT	[dbo].[FNC_DuracaoEtapaProducao](1) AS Resultado,
-								DATEDIFF(MILLISECOND, @DataInicio, GETDATE()) AS Tempo_Execucao	
+						SELECT	[dbo].[FNC_CalcularTempoProducaoProduto](1) AS Resultado,
+								DATEDIFF(MILLISECOND, @DataInicio, GETDATE()) AS TempoExecucao	
 	*/
 	BEGIN	
 		--Criando variavel.
 		DECLARE	@Duracao SMALLINT
 
 		--Setando variável para receber o tempo somado das etapas de produção do produto escolhido.
-		SELECT	@Duracao	= SUM(ep.Duracao)
+		SELECT	@Duracao = SUM(ep.Duracao)
 			FROM [dbo].[EtapaProducao] ep WITH(NOLOCK)							
 			WHERE @Id = ep.IdProduto
 
-		--Retorno da da variavel.
+		--Retorno da variavel.
 		RETURN @Duracao
 	END
 GO
