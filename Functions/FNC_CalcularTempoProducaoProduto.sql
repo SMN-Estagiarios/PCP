87617e9b7325e49b49d3d9c912f094f944e38c3e
Orcino Neto <orcinonetojp@gmail.com>
2024-05-27 10:26:47 -0300

    Refatorando a funcao

--- Functions/FNC_CalcularTempoProducaoProduto.sql
+++ Functions/FNC_CalcularTempoProducaoProduto.sql
@@ -4,28 +4,27 @@ CREATE OR ALTER FUNCTION [dbo].[FNC_CalcularTempoProducaoProduto](@Id INT)
 	/*
 	Documentação
 	Arquivo Fonte....: FNC_CalcularTempoProducaoProduto.sql
-	Objetivo.........: Função para calcular o tempo de produção do produto selecionado.
+	Objetivo.........: Funcao para calcular o tempo de produção do produto selecionado.
 	Autor............: OrcinoNeto
 	Data.............: 21/05/2024
 	Ex...............: 
 						DBCC DROPCLEANBUFFERS;
 						DBCC FREEPROCCACHE;
 						
-						DECLARE @Dat_ini DATETIME = GETDATE()
-						SELECT	[dbo].[FNC_DuracaoEtapaProducao](2) AS Resultado,
-								DATEDIFF(millisecond, @Dat_ini, GETDATE()) AS Tempo_Execucao	
+						DECLARE @DataInicio DATETIME = GETDATE()
+						SELECT	[dbo].[FNC_DuracaoEtapaProducao](1) AS Resultado,
+								DATEDIFF(MILLISECOND, @DataInicio, GETDATE()) AS Tempo_Execucao	
 	*/
 	BEGIN	
+		--Criando variavel.
 		DECLARE	@Duracao SMALLINT
 
+		--Setando variável para receber o tempo somado das etapas de produção do produto escolhido.
 		SELECT	@Duracao	= SUM(ep.Duracao)
-			FROM [dbo].[EtapaProducao] ep WITH(NOLOCK)
-				INNER JOIN [dbo].[Produto] p WITH(NOLOCK)
-					ON ep.IdProduto = p.Id
-				INNER JOIN [dbo].[PedidoProduto] pp WITH(NOLOCK)
-					ON pp.IdProduto = p.Id
-			WHERE @Id = pp.IdPedido
+			FROM [dbo].[EtapaProducao] ep WITH(NOLOCK)							
+			WHERE @Id = ep.IdProduto
 
+		--Retorno da da variavel.
 		RETURN @Duracao
 	END
 GO
