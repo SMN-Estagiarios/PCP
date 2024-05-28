0192cfa733e2e3e540dac3c2b9578f3d5e3a7a29
João Victor Maia <maia.joaovic@gmail.com>
2024-05-28 08:59:35 -0300

    fix: correção de cálculo do estoque faltante

--- Triggers/TRG_ChecarMateriaPrimaProducao.sql
+++ Triggers/TRG_ChecarMateriaPrimaProducao.sql
@@ -7,6 +7,7 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_ChecarMateriaPrimaProducao]
         Arquivo Fonte.........: TRG_ChecarMateriaPrimaProducao.sql
         Objetivo..............: Trigger para checar no início da produção se há materia-prima suficiente para finalizar o produto.
                                 Caso não haja, será feita a requisição da matéria-prima faltante
+                                //ep.NumeroEtapa fixado em 1 pois será sempre o início da produção
         Autor.................: João Victor Maia
         Data..................: 24/05/2024
         Exemplo...............: BEGIN TRAN
@@ -14,14 +15,14 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_ChecarMateriaPrimaProducao]
                                             c.Quantidade AS QuantidadeNecessaria,
                                             c.IdMateriaPrima,
                                             emp.QuantidadeFisica
-                                        FROM [dbo].[Composicao] c
-                                            INNER JOIN [dbo].[EstoqueMateriaPrima] emp
+                                        FROM [dbo].[Composicao] c WITH(NOLOCK)
+                                            INNER JOIN [dbo].[EstoqueMateriaPrima] emp WITH(NOLOCK)
                                                 ON c.IdMateriaPrima = emp.IdMateriaPrima
                                         WHERE IdProduto = 1
 
                                     SELECT  IdEstoqueMateriaPrima,
                                             Quantidade
-                                        FROM [dbo].[MovimentacaoEstoqueMateriaPrima]
+                                        FROM [dbo].[MovimentacaoEstoqueMateriaPrima] WITH(NOLOCK)
                                         WHERE IdEstoqueMateriaPrima IN (1, 2, 3)
 
                                     INSERT INTO [dbo].[Producao] (IdEtapaProducao, IdPedidoProduto, Quantidade, DataInicio, DataTermino)
@@ -31,15 +32,15 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_ChecarMateriaPrimaProducao]
                                             c.Quantidade AS QuantidadeNecessaria,
                                             c.IdMateriaPrima,
                                             emp.QuantidadeFisica
-                                        FROM [dbo].[Composicao] c
-                                            INNER JOIN [dbo].[EstoqueMateriaPrima] emp
+                                        FROM [dbo].[Composicao] c WITH(NOLOCK)
+                                            INNER JOIN [dbo].[EstoqueMateriaPrima] emp WITH(NOLOCK)
                                                 ON c.IdMateriaPrima = emp.IdMateriaPrima
                                         WHERE IdProduto = 1
 
                                     SELECT  IdEstoqueMateriaPrima,
                                             Quantidade,
                                             IdTipoMovimentacao
-                                        FROM [dbo].[MovimentacaoEstoqueMateriaPrima]
+                                        FROM [dbo].[MovimentacaoEstoqueMateriaPrima] WITH(NOLOCK)
                                         WHERE IdEstoqueMateriaPrima IN (1, 2, 3)
                                 ROLLBACK TRAN
     */
@@ -52,8 +53,10 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_ChecarMateriaPrimaProducao]
 
         --Checar se a produção será iniciada agora
         IF NOT EXISTS   (SELECT TOP 1 1
-                            FROM Inserted
-                            WHERE IdEtapaProducao = 1
+                            FROM Inserted i
+                                INNER JOIN [dbo].[EtapaProducao] ep WITH(NOLOCK)
+                                    ON i.EtapaProducao = ep.Id
+                            WHERE ep.NumeroEtapa = 1
                         )
             BEGIN
                 RETURN
@@ -70,13 +73,13 @@ CREATE OR ALTER TRIGGER [dbo].[TRG_ChecarMateriaPrimaProducao]
         INSERT INTO #MateriaPrima
             SELECT  c.IdMateriaPrima,
                     c.Quantidade AS QuantidadeNecessaria,
-                    i.Quantidade * (c.Quantidade - emp.QuantidadeFisica) AS QuantidadeFaltante
+                    i.Quantidade * c.Quantidade - emp.QuantidadeFisica AS QuantidadeFaltante --Calcular o que será usado menos o que ja tem no estoque
                 FROM Inserted i
-                    INNER JOIN [dbo].[EtapaProducao] ep
-                        ON i.IdEtapaProducao = ep.Id
-                    INNER JOIN [dbo].[Composicao] c
+                    INNER JOIN [dbo].[EtapaProducao] ep WITH(NOLOCK)
+                        ON i.IdEtapaProducao = ep.Id 
+                    INNER JOIN [dbo].[Composicao] c WITH(NOLOCK)
                         ON ep.IdProduto = c.IdProduto
-                    INNER JOIN [dbo].[EstoqueMateriaPrima] emp
+                    INNER JOIN [dbo].[EstoqueMateriaPrima] emp WITH(NOLOCK)
                         ON c.IdMateriaPrima = emp.IdMateriaPrima
 
         --Loopar registros da tabela temporária
